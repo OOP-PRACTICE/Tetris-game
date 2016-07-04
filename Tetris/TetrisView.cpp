6a25d202406242f9d82bec3025878be5d79348ca
Chaoyang Liu <chaoyanglius@gmail.com>
2016-07-04 13:25:22 +0800

    Add project files.

---
+++ Tetris/TetrisView.cpp
@@ -0,0 +1,83 @@
+
+// TetrisView.cpp : CTetrisView 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "Tetris.h"
+#endif
+
+#include "TetrisDoc.h"
+#include "TetrisView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CTetrisView
+
+IMPLEMENT_DYNCREATE(CTetrisView, CFormView)
+
+BEGIN_MESSAGE_MAP(CTetrisView, CFormView)
+END_MESSAGE_MAP()
+
+// CTetrisView 构造/析构
+
+CTetrisView::CTetrisView()
+	: CFormView(IDD_TETRIS_FORM)
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CTetrisView::~CTetrisView()
+{
+}
+
+void CTetrisView::DoDataExchange(CDataExchange* pDX)
+{
+	CFormView::DoDataExchange(pDX);
+}
+
+BOOL CTetrisView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CFormView::PreCreateWindow(cs);
+}
+
+void CTetrisView::OnInitialUpdate()
+{
+	CFormView::OnInitialUpdate();
+	GetParentFrame()->RecalcLayout();
+	ResizeParentToFit();
+
+}
+
+
+// CTetrisView 诊断
+
+#ifdef _DEBUG
+void CTetrisView::AssertValid() const
+{
+	CFormView::AssertValid();
+}
+
+void CTetrisView::Dump(CDumpContext& dc) const
+{
+	CFormView::Dump(dc);
+}
+
+CTetrisDoc* CTetrisView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CTetrisDoc)));
+	return (CTetrisDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CTetrisView 消息处理程序
---
+++ Tetris/TetrisView.h
@@ -0,0 +1,52 @@
+
+// TetrisView.h : CTetrisView 类的接口
+//
+
+#pragma once
+
+
+class CTetrisView : public CFormView
+{
+protected: // 仅从序列化创建
+	CTetrisView();
+	DECLARE_DYNCREATE(CTetrisView)
+
+public:
+#ifdef AFX_DESIGN_TIME
+	enum{ IDD = IDD_TETRIS_FORM };
+#endif
+
+// 特性
+public:
+	CTetrisDoc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+	virtual void OnInitialUpdate(); // 构造后第一次调用
+
+// 实现
+public:
+	virtual ~CTetrisView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // TetrisView.cpp 中的调试版本
+inline CTetrisDoc* CTetrisView::GetDocument() const
+   { return reinterpret_cast<CTetrisDoc*>(m_pDocument); }
+#endif
+
